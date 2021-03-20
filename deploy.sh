aa2570eb06363bb0fe4805cbbe8fdfb41ea989c4
Fer González <fernandagonzalez@ufm.edu>
2021-03-20 01:47:09 +0000

    Header file and syscall table file added. Deploy changes.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-# Compile and "deploy" a new custom kernel from source on Arch Linux
+# Compile and "deploy" a new custom kernel from source on Ubuntu Linux
 
 # Change this if you'd like. It has no relation
 # to the suffix set in the kernel config.
@@ -10,18 +10,14 @@ set -e
 
 # Compile the kernel
 make -j4
+
 # Compile and install modules
 make -j4 modules_install
 
 # Install kernel image
-cp arch/x86_64/boot/bzImage /boot/vmlinuz-linux$SUFFIX
-make -j4 modules_install install
+#cp arch/x86_64/boot/bzImage /boot/vmlinuz-linux$SUFFIX
+#make -j4 modules_install install
+make -j4 install
 
-# # Create preset and build initramfs
-# sed s/linux/linux$SUFFIX/g \
-#     </etc/mkinitcpio.d/linux.preset \
-#     >/etc/mkinitcpio.d/linux$SUFFIX.preset
-# mkinitcpio -p linux$SUFFIX
-
-# # Update bootloader entries with new kernels.
-# grub-mkconfig -o /boot/grub/grub.cfg
+# Update the bootloader
+sudo update-grub
